9091813fe70a8eb0c530c364074af2dfd407a220
Ernestas Poskus <ernestas.poskus@oxid-esales.com>
2013-12-19 15:39:16 +0200

    Postgres: rails user and databases added

--- puppet/manifests/install_postgres.pp
+++ puppet/manifests/install_postgres.pp
@@ -5,23 +5,38 @@
 
 class install_postgres 
 {
-
-	$db_user = 'vag'
+	$db_user = 'vagrant'
+	$rails_user = 'out'
 
 	class {'postgresql': }
 	class {'postgresql::server':
 		version => '9.1',
 	}->
+	
+	
 	pg_user { $db_user:
     	ensure   => present,
     	password => $db_user,
-	}->
-	pg_database {'rails_db1':
+	}
+	pg_user { $rails_user:
+    	ensure   => present,
+    	password => $rails_user,
+	}
+
+	
+	pg_database { $db_user:
 	    ensure   => present,
 	    owner    => $db_user,
 	    encoding => 'UTF8',
     	locale   => 'en_US.UTF-8',
 	    require  => Pg_user[$db_user],
+	}
+	pg_database { $rails_user:
+	    ensure   => present,
+	    owner    => $rails_user,
+	    encoding => 'UTF8',
+    	locale   => 'en_US.UTF-8',
+	    require  => Pg_user[$rails_user],
 	}
 }
 

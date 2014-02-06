9783bc7cf75b67e9eb98ca4732705795175c6b38
unknown <ernestas.poskus@gmail.com>
2014-02-07 00:10:48 +0200

    Zsh dependency: git

--- puppet/manifests/helpers/install_zsh.pp
+++ puppet/manifests/helpers/install_zsh.pp
@@ -2,35 +2,37 @@
 #
 #
 
-class install_zsh(oh_my = true, $username = 'vagrant')
+class install_zsh($oh_my = true, $username = 'vagrant')
 {
     package { 'zsh':
         ensure => present,
-        require => Exec['out_apt_update'],
+        require => [ Exec['out_apt_update'], Package['git-core'] ],
     }
 
 	$home_path = "/home/${username}/"
 
+	if $oh_my
+	{
+		# Changing to User shell to ZSH
+		user { "zsh_user_${username}":
+			ensure  => present,
+			name    => $username,
+			shell   => '/usr/bin/zsh',
+			require => Package['zsh']
+		}
+		
+		exec { 'clone_oh_my_zsh':
+			command => "git clone git://github.com/ernestas-poskus/oh-my-zsh.git ${home_path}.oh-my-zsh",
+			creates => "${home_path}.oh-my-zsh",
+			user 	=> $username,
+			unless  => "ls ${home_path}.oh-my-zsh",
+			require => User["zsh_user_${username}"] 
+		}
 
-	# Changing to User shell to ZSH
-	user { "zsh_user_${username}":
-		ensure  => present,
-		name    => $username,
-		shell   => '/usr/bin/zsh',
-		require => Package['zsh']
-	}
-	
-	exec { 'clone_oh_my_zsh':
-		command => "git clone git://github.com/ernestas-poskus/oh-my-zsh.git ${home_path}.oh-my-zsh",
-		creates => "${home_path}.oh-my-zsh",
-		user 	=> $username,
-		unless  => "ls ${home_path}.oh-my-zsh",
-		require => User["zsh_user_${username}"] 
-	}
-
-	exec { 'copy-zshrc':
-		command => "cp ${home_path}.oh-my-zsh/templates/zshrc.zsh-out13 ${home_path}.zshrc",
-		user 	=> $username,
-		require => Exec['clone_oh_my_zsh'],
+		exec { 'copy-zshrc':
+			command => "cp ${home_path}.oh-my-zsh/templates/zshrc.zsh-out13 ${home_path}.zshrc",
+			user 	=> $username,
+			require => Exec['clone_oh_my_zsh'],
+		}
 	}
 }
